4a7c76e09618eb1c5baa0dfb212344e36eea70eb
= <carlton.upperdine@bjss.com>
2023-03-08 11:05:31 +0000

    working example

--- examples/main.ts
+++ examples/main.ts
@@ -3,8 +3,16 @@ import { Router } from 'https://deno.land/x/muno@v0.1.0/router/router.ts';
 
 const router = new Router();
 
-router.get('/', (_req: Request) => {
+router.get('/hello', (_req: Request) => {
     return new Response('Hello, World!');
 });
 
-http.serve(router.handler());
+router.get('/hello/:name', (req: Request) => {
+    const pattern = new URLPattern({ pathname: '/hello/:name' });
+    const match = pattern.exec(req.url);
+    return new Response(
+        `Hello, ${match?.pathname.groups.name}!`,
+    );
+});
+
+await http.serve(router.handler());
